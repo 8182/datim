b10e8c09a58f3ee2649a4087b08677e5e9f7aceb
lulzx <wiserishabh@gmail.com>
2019-03-23 12:17:09 +0530

    flake8 suggestions

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 from http.server import BaseHTTPRequestHandler
 from cowpy import cow
+
+
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
         message = cow.Cowacter().milk('Moo!')
         self.wfile.write(message.encode())
